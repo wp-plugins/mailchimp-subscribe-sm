86e6734799febb6f5978b2af1784b0e45ff8a55a
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 07:13:09 +0000

    Few Changes
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-subscribe-sm/tags/1.1@1044739 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form_2.php
+++ form_2.php
@@ -99,14 +99,7 @@
 		margin-left: -2% !important;
 
 		background-color: #7abcff !important;
-		background: rgb(122,188,255) !important; /* Old browsers */
-		background: -moz-linear-gradient(top,  rgba(122,188,255,1) 0%, rgba(96,171,248,1) 44%, rgba(64,150,238,1) 100%) !important; /* FF3.6+ */
-		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(122,188,255,1)), color-stop(44%,rgba(96,171,248,1)), color-stop(100%,rgba(64,150,238,1))); /* Chrome,Safari4+ */
-		background: -webkit-linear-gradient(top,  rgba(122,188,255,1) 0%,rgba(96,171,248,1) 44%,rgba(64,150,238,1) 100%); /* Chrome10+,Safari5.1+ */
-		background: -o-linear-gradient(top,  rgba(122,188,255,1) 0%,rgba(96,171,248,1) 44%,rgba(64,150,238,1) 100%); /* Opera 11.10+ */
-		background: -ms-linear-gradient(top,  rgba(122,188,255,1) 0%,rgba(96,171,248,1) 44%,rgba(64,150,238,1) 100%); /* IE10+ */
-		background: linear-gradient(to bottom,  rgba(122,188,255,1) 0%,rgba(96,171,248,1) 44%,rgba(64,150,238,1) 100%); /* W3C */
-		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#7abcff', endColorstr='#4096ee',GradientType=0 ); /* IE6-9 */
+		
 	}
 	#sm_form > input[type="text"],#sm_form >input[type="email"]{
 		margin: 0 auto !important;
@@ -150,6 +143,7 @@
 				 <input type="submit"  name="submit" value="<?php echo get_post_meta($post->ID,'sm_form_cta_text',true); ?>" class="sm_submit" id='sm_submit' />
 
 			</p>
+			<input style='display:none;' type='email' id='sm_name' class='sm_field' name='sm_name' placeholder='Email' required >
 		</form>
 		<span id="response">
 			<?php require_once('inc/store-address.php'); if($_GET['submit']){ echo storeAddress(); } ?>
