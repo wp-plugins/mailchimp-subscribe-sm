eebaabcd194ef9a7e7ed085ffd21f460793c9383
umarbajwa <umarbajwa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 16:52:45 +0000

    FewChanges
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-subscribe-sm/tags/1.1@1067216 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subcribe-me.php
+++ subcribe-me.php
@@ -11,10 +11,10 @@ License: GPL V2
 
 
 *********
-* Copyright: 2014 http://web-settler.com/
+* Copyright: 2014 http://web-settler.com
 * Note: License Free to use, Free to modify, Permission required to redistribute.
-* Help URL:http://web-settler.com/contact/
-* Support: http://web-settler.com/ulp-support/
+* Help URL:http://web-settler.com/contact
+* Support: http://web-settler.com/ulp-support
 * Email: umar@web-setter.com
 ********
 */
